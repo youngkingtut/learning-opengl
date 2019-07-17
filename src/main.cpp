f557b9c3dc295340ce85bdcd8000629b31747f23
Tristan Storz <tristanstorz@gmail.com>
2019-07-16 21:08:00 -0600

    Works on mac

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
-#include "Shaders/ShaderCompiler.hpp"
+#include "src/ShaderCompiler/ShaderCompiler.hpp"
 
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
@@ -20,6 +20,7 @@ int main() {
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
     GLFWwindow* window = glfwCreateWindow(800, 600, "LeraningOpenGL", nullptr, nullptr);
 
